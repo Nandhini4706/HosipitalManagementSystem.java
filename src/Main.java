500f8b8f01e812c3f17a74054a90f80486705fd9
Nandhini4706 <nandhinimm4706@gmail.coom>
2025-11-27 15:00:04 +0530

    patient view table build

--- src/Main.java
+++ src/Main.java
@@ -15,29 +15,71 @@ public class Main {
             System.out.println("Access Denied");
         }
 
-        while(true) {
-            System.out.println("-------------Menu----------------");
-            System.out.println("1. Add Patient Details");
-            System.out.println("2. Doctor Details");
-            System.out.println("3. Nurse Details");
-            System.out.println("Exit");
-            System.out.println("Enter Your Choice : ");
-            int n=sc.nextInt();
-            sc.nextLine();
+            ArrayList<String> patientName= new ArrayList<>();
+            ArrayList<String> patientCondition=new ArrayList<>();
+            ArrayList<String> date=new ArrayList<>();
+        ArrayList<String> docName=new ArrayList<>();
+        ArrayList<String> doctorSpecs=new ArrayList<>();
+        ArrayList<String> nurseName=new ArrayList<>();
+        ArrayList<String> nurseShift=new ArrayList<>();
+            while (true) {
+                System.out.println("-------------Menu----------------");
+                System.out.println("1. Add Patient Details");
+                System.out.println("2. Doctor Details");
+                System.out.println("3. Nurse Details");
+                System.out.println("4. View table ");
+                System.out.println("Enter Your Choice : ");
+                int n = sc.nextInt();
+                sc.nextLine();
 
-            switch(n){
-                case 1:
-                    System.out.println("Enter Patient Name ");
-                    String name=sc.nextLine();
-                    System.out.println("Condition :");
-                    String cat=sc.nextLine();
+                switch (n) {
+                    case 1:
+                        System.out.println("Enter Patient Name ");
+                        String name=sc.nextLine();
 
-                    break;
-                case 2:
-                    System.out.println("Enter Doctor Name ");
-                    String name=sc.nextLine();
-                    break;
+                        System.out.println("Condition :");
+                        String cat = sc.nextLine();
+                        System.out.println("Enter Date Of Admit :");
+                        String tim=sc.nextLine();
+
+                        patientName.add(name);
+                        patientCondition.add(cat);
+                        date.add(tim);
+
+                        break;
+                    case 2:
+                        System.out.println("Enter Doctor Name ");
+                        String nam = sc.nextLine();
+                        System.out.println("Specialist  ");
+                        String spec = sc.nextLine();
+
+                        docName.add(nam);
+                        doctorSpecs.add(spec);
+                        break;
+
+                    case 3:
+                        System.out.println("Enter Nurse Name : ");
+                        String nur = sc.nextLine();
+                        System.out.println("Enter Shift Type : ");
+                        String shift = sc.nextLine();
+
+                        nurseName.add(nur);
+                        nurseShift.add(shift);
+                        break;
+
+                    case 4:
+                        System.out.println("\n--------------------------------------------------------------------");
+                        System.out.printf("| %-10s | %-15s | %-12s |", "Patient Name", "Condition","Admit Date");
+                        System.out.println("\n--------------------------------------------------------------------");
+
+                        for(int i=0; i<patientName.size(); i++){
+                            System.out.printf("|%-10s | %-15s | %-12s |\n", patientName.get(i), patientCondition.get(i), date.get(i));
+
+                        }
+                        System.out.println("----------------------------------------------------------------------");
+                        break;
+
+                }
             }
         }
     }
-}
