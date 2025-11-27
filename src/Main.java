3f0f3c393cb60e5d3c8a6fd71435abd9f683d7f7
Nandhini4706 <nandhinimm4706@gmail.coom>
2025-11-27 13:16:20 +0530

    setup patient and doctor

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        System.out.println("\n Welcome To Hospital Management");
+        System.out.println("Enter id : ");
+        String id = sc.nextLine();
+        String id2 = "Vellu";
+
+        if (id.equals(id2)) {
+            System.out.println("Access Granted");
+        } else {
+            System.out.println("Access Denied");
+        }
+
+        while(true) {
+            System.out.println("-------------Menu----------------");
+            System.out.println("1. Add Patient Details");
+            System.out.println("2. Doctor Details");
+            System.out.println("3. Nurse Details");
+            System.out.println("Exit");
+            System.out.println("Enter Your Choice : ");
+            int n=sc.nextInt();
+            sc.nextLine();
+
+            switch(n){
+                case 1:
+                    System.out.println("Enter Patient Name ");
+                    String name=sc.nextLine();
+                    System.out.println("Condition :");
+                    String cat=sc.nextLine();
+
+                    break;
+                case 2:
+                    System.out.println("Enter Doctor Name ");
+                    String name=sc.nextLine();
+                    break;
+            }
         }
     }
 }
